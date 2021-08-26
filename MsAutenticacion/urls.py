dbc7ca05d9cc2b88ec9532346a57a066cea5695d
felipevipa <felipevipa@outlook.com>
2021-08-26 17:17:41 -0500

    Agregado endpoint para crear usuario

--- MsAutenticacion/urls.py
+++ MsAutenticacion/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-from autenticacionApp.views import VerifyTokenView
+from autenticacionApp.views import registroView, VerifyTokenView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
-    path('token/verify/', VerifyTokenView.as_view())
+    path('token/verify/', VerifyTokenView.as_view()),
+    path('users/', registroView.as_view())
 ]
--- autenticacionApp/views.py
+++ autenticacionApp/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenVerifyView
 from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework.views import APIView
+from .models import hotelUser
 
 class VerifyTokenView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
@@ -19,3 +21,19 @@ class VerifyTokenView(TokenVerifyView):
             raise InvalidToken(e.args[0])
 
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+class registroView(APIView):
+    def post(self, request, *args, **kwargs):
+        data = request.data
+
+        print(data)
+
+        if(len(data['username']) > 3 and len(data['password']) > 4):
+            user = hotelUser.objects.create_user(data['username'], data['password'])
+            user.save()
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            error = {
+                'message': 'Datos invalidos'
+            }
+            return Response(error, status=status.HTTP_400_BAD_REQUEST)
